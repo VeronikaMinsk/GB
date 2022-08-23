a32583e48cb4938d51a56397196596a87652f959
Veronika <griva.verona@gmail.com>
2022-08-24 00:02:49 +0300

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 5: Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int y = -1 * x; y <= x; y += 1)
+Console.WriteLine(y);
