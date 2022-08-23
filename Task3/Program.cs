bdd1e432087ad493a9f99cedcc8597817546c1c4
Veronika <griva.verona@gmail.com>
2022-08-24 00:04:56 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным
+
+Console.WriteLine("введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+Console.WriteLine("четное число ");  
+else
+ Console.WriteLine("нечетное число ");
